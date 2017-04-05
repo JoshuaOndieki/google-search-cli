090b7076cf9643bc10f6cb8e6ce21880fe5bb752
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-04-05 12:03:31 +0300

    Test does not return None and instance is dict

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
 import unittest
 
 class TestGoogleSearchApi(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.search_instance=search("Test Driven Development")
+
+    def test_it_does_not_return_None(self):
+        self.assertIsNotEqual(search_instance,None,msg="Should not return None")
+
+    def test_search_returns_a_multidimensional_dictionary(self):
+        self.assertIsinstance(search_instance,dict,msg="Search request should returns json in dictionary format")
 
 
 
