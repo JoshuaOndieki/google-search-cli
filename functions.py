38e9947d252a6fdc23274420494252b2cc64e898
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-04-06 19:18:41 +0300

    Use try-except to handle connection errors

--- functions.py
+++ functions.py
@@ -6,7 +6,10 @@ class GoogleSearchApi():
         googleaccesskey='AIzaSyAkFYTMOVmvPniL4Y9dL5RDQehQ7BJ6gCQ'
         cx='014930882088392870627:heixbbhopvu'
         search_term=googlecustomsearch+'key='+googleaccesskey+'&cx='+cx+'&q='+search_term
-        search_data=requests.get(search_term,timeout=10)
-        search_data = search_data.json()
-
+        try:
+            search_data=requests.get(search_term,timeout=10)
+            search_data = search_data.json()
+        except Exception:
+            return "GSA couldn't connect to the internet\n Please check your internet connection and try again.\n Happy searching!"
+        
         return search_data
